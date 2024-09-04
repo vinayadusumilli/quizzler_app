e2b985802c07820709e23d1ae45a65516afe658d
Vinay Adusumilli <146542274+vinayadusumilli@users.noreply.github.com>
2024-09-04 22:57:56 +0100

    Completed basic User Interface(UI)

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from ui import Ui
 api_data = Data()
 data = api_data.get_data()
 quiz_category = data[0]["category"]
-ui = Ui(quiz_category=quiz_category)
+quiz_question = {data[0]["question"]: data[0]["correct_answer"]}
+ui = Ui(quiz_category=quiz_category, quiz_question=quiz_question)
--- ui.py
+++ ui.py
@@ -1,13 +1,23 @@
 from tkinter import *
+from typing import Final
+
+FONT: Final[tuple] = ("Arial", 10, "bold")
 
 
 class Ui:
-    def __init__(self, quiz_category) -> None:
+    def __init__(self, quiz_category, quiz_question) -> None:
         self.quiz_category = quiz_category
+        self.quiz_question = quiz_question
         self.make_screen()
 
     def make_screen(self):
+        # Window setup
         window = Tk()
         window.title(self.quiz_category)
+        window.config(padx=50, pady=100)
+        # Canvas setup
+        canvas = Canvas(width=600, height=600, bg="green")
+        canvas.create_text(300,200, text=self.quiz_question, fill="white", font=FONT)
+        canvas.grid(row=0, column=0)
 
         window.mainloop()
